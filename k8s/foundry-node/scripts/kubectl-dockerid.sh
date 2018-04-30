0cd9dacc0bf94d5b6758ebdeb732e4fefcf448b3
Matt Jeanneret <mj3580@att.com>
2018-04-30 10:57:10 -0400

    Updated yaml files, daemon.json, and install instructions to reflect fully qualified repo name.  Added single instance perisisted etcd instance.  Scripts for quick environment buildup and teardown.  Script to workaround voltha to etcd startup bug.

--- k8s/foundry-node/scripts/kubectl-dockerid.sh
+++ k8s/foundry-node/scripts/kubectl-dockerid.sh
@@ -4,5 +4,6 @@ NAMESPACE=$1
 POD_NAME=$2
 
 set -x
-kubectl get pod $POD_NAME -n $NAMESPACE -o jsonpath="{.status.hostIP} {.status.containerStatuses[*].containerID}{'\n'}"
+#kubectl get pod $POD_NAME -n $NAMESPACE -o jsonpath="{.status.hostIP} {.status.containerStatuses[*].containerID}{'\n'}"
+kubectl get pod $POD_NAME -n $NAMESPACE -o jsonpath="{.status.containerStatuses[*].containerID}{'\n'}" | cut -d'/' -f3
 
