c94d82cc434bdc6a67aa0170e14e965f6ef43b91
nick <np202g@att.com>
2018-05-23 17:48:31 -0400

    newest voltha docker image set in vcore yaml files (consul and etcd) updated onos config compliant with new naming convention upgraded script for building and uploading voltha doker image
    Change-Id: I788e28160cb5431a031899e4ab3d7ea5eb1b3db1

--- tag_and_ship.sh
+++ tag_and_ship.sh
@@ -1,4 +1,4 @@
 . env.sh
-make build
+make voltha
 docker tag voltha-voltha docker-repo.dev.atl.foundry.att.com:5000/voltha-voltha:${1}
 docker push docker-repo.dev.atl.foundry.att.com:5000/voltha-voltha:${1}
