75d25e5e2df4304b5f4baaf6f313e6cab41b19a0
Nicolas Palpacuer <np202g@att.com>
2018-06-26 16:17:15 -0400

    pointing netcfg-update to reference network-cfg.json file
    Change-Id: Ia236e97c113a743a7675c31203985f1e4301840c

--- k8s/foundry-node/foundry-k8s-cluster/netcfg-update.sh
+++ k8s/foundry-node/foundry-k8s-cluster/netcfg-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --user onos:rocks -X POST -H "Content-Type: application/json" http://onos-voltha-ui.voltha.svc.cluster.local:8181/onos/v1/network/configuration/ -d @network-cfg.json
+curl --user onos:rocks -X POST -H "Content-Type: application/json" http://onos-voltha-ui.voltha.svc.cluster.local:8181/onos/v1/network/configuration/ -d @../../../onos-config/network-cfg.json
 
