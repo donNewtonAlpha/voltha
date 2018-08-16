86474b584c67934dcba6d736e915b2a245a3ee74
Matt Jeanneret <mj3580@att.com>
2018-08-16 08:18:58 -0400

    Remove helm delete for voltha-kafka.  Update onos script a bit

--- k8s/foundry-node/foundry-k8s-cluster/quick-onos-update.sh
+++ k8s/foundry-node/foundry-k8s-cluster/quick-onos-update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-updfile=$1
+host=$1
+updfile=$2
 
-if [ -n "$updfile" ]
+if [ -n "$updfile" ] && [ -n "$host" ]
 then
   set -x
-  curl -v --user onos:rocks -X POST -H "Content-Type: application/json" http://10.64.1.124:30120/onos/v1/network/configuration/ -d @$updfile
+  curl -v --user onos:rocks -X POST -H "Content-Type: application/json" http://${host}:30120/onos/v1/network/configuration/ -d @$updfile
 else
-  echo "$0 <onos-netcfg-update-json file>"
+  echo "$0 <onos-host> <onos-netcfg-update-json file>"
 fi
